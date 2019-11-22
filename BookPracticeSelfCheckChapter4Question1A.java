ef0bf2f49909951aab9928cabeece6d48707743a
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-11-22 17:44:31 -0600

    Create BookPracticeSelfCheckChapter4Question1A.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 4, BJP5 self check question 4:1, dated on November 19, 2019.

---
+++ BookPracticeSelfCheckChapter4Question1A.java
@@ -0,0 +1,12 @@
+public class BookPracticeSelfCheckChapter4Question1A {
+    public static void main(String[] args) {
+        String z = "odd";
+        if (z.equals("odd")) {
+            System.out.println("z is odd.");
+        } else {
+            System.out.println("z is not odd.");
+        }
+
+
+    }
+}
