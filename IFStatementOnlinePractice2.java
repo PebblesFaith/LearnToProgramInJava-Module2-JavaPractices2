7cede7b4fe2f9c55ef905dee884d3f3a8c4ddbc0
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-11-22 17:47:30 -0600

    Create IFStatementOnlinePractice2.java
    I have completed, my online tuturial on If/Else Statement dated on November 19, 2019.

---
+++ IFStatementOnlinePractice2.java
@@ -0,0 +1,10 @@
+public class IFStatementOnlinePractice2 {
+    public static void main(String[] args) {
+        int numberEntered = 12346;
+        if (numberEntered != 12345) {
+            System.out.println("you entered the magic number!");
+        } else {
+            System.out.println("Sorry, that is not right!");
+        }
+    }
+}
