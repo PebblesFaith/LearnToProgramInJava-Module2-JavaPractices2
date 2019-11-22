5f3efd414cca39761e0f1c1e7b6d68cfc3d61d18
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-11-22 17:47:10 -0600

    Create IFElseStatementOnlinePractice2.java
    I have completed, my online tuturial on If/Else Statemen dated on November 19, 2019.

---
+++ IFElseStatementOnlinePractice2.java
@@ -0,0 +1,14 @@
+public class IFElseStatementOnlinePractice2 {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x > 10) {
+            System.out.println("a");
+        } else if (x > 50) {
+            System.out.println("b");
+        } else if (x > 10) {
+            System.out.println("c");
+        } else {
+            System.out.println("d");
+        }
+    }
+}
