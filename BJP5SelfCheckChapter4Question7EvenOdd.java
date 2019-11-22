aa3440226d24860ce7c1af7d3663ea8adbec9eb6
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-11-22 17:44:09 -0600

    Create BJP5SelfCheckChapter4Question7EvenOdd.java
    I have completed, my "Building Java Program" fifth edition textbook Chapter 4, BJP5 self check question 4:7, dated on November 19, 2019.

---
+++ BJP5SelfCheckChapter4Question7EvenOdd.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class BJP5SelfCheckChapter4Question7EvenOdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter any number: ");
+        double testnumber = input.nextDouble();
+
+
+
+        if ((testnumber + testnumber)  / 2  % 2 == 0 )  {
+            System.out.println("The " + testnumber + " you have entered is an even number.");
+        } else {
+            System.out.println("The " + testnumber + " you have entered is an odd number.");
+        }
+
+    }
+}
