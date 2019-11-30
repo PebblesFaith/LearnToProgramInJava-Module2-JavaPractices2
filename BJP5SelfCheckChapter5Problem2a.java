d65facd3514e005595b7c1a94da7d244a7c4dcf9
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-11-29 22:50:46 -0600

    Create BJP5SelfCheckChapter5Problem2a.java
    Convert each of the following for loops into an equivalent while loop.
    
        In chapter 5, Self Check number 2 - problem a on page 377 and 378 from my
        "Building Java Programs, A Back to Basics Approach 5e" textbook" for my online course
        "Learn to Program in Java" dated on November 29, 2019.
    
        I had completed, the Self Check, Problem 2:a “For To While” based on the best possible textbook
        instructions posted onto “GitHub” website, as the correct answer key dated on November 29, 2019.
    
       Completed by Sarai Hannah Ajai

---
+++ BJP5SelfCheckChapter5Problem2a.java
@@ -0,0 +1,22 @@
+/*
+    Convert each of the following for loops into an equivalent while loop.
+
+    In chapter 5, Self Check number 2 - problem a on page 377 and 378 from my
+    "Building Java Programs, A Back to Basics Approach 5e" textbook" for my online course
+    "Learn to Program in Java" dated on November 29, 2019.
+
+    I had completed, the Self Check, Problem 2:a “For To While” based on the best possible textbook
+    instructions posted onto “GitHub” website, as the correct answer key dated on November 29, 2019.
+
+   Completed by Sarai Hannah Ajai
+ */
+public class BJP5SelfCheckChapter5Problem2a {
+    public static void main (String[] args) {
+        int max = 5;
+        int n = 1;
+        while (n <= max) {
+            System.out.println(n);
+            n++;
+        }
+    }
+}
