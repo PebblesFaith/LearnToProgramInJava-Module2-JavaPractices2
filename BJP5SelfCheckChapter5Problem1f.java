a7865885e77afd5f2b603df1503ff1fb17c5150c
Sarai H Ajai <ajai.sarai.h@outlook.com>
2019-11-29 21:11:53 -0600

    Create BJP5SelfCheckChapter5Problem1f.java
    For each of the following while loops, state how many times the loop will execute its body.
        Remember that "zero," "infinity," and "unknown" are legal answers. Also, what is the output
        of the code in each case? In chapter 5, Self Check number 1 - problem f on page 376 and 377 from my
        "Building Java Programs, A Back to Basics Approach 5e" textbook" for my online course
        "Learn to Program in Java" dated on November 29, 2019.
    
        I had completed, the Self Check, Problem 1:f “While Loops” based on the best possible textbook
        instructions posted onto “GitHub” website, as the correct answer key dated on November 29, 2019.
    
       Completed by Sarai Hannah Ajai

---
+++ BJP5SelfCheckChapter5Problem1f.java
@@ -0,0 +1,22 @@
+/*
+    For each of the following while loops, state how many times the loop will execute its body.
+    Remember that "zero," "infinity," and "unknown" are legal answers. Also, what is the output
+    of the code in each case? In chapter 5, Self Check number 1 - problem f on page 376 and 377 from my
+    "Building Java Programs, A Back to Basics Approach 5e" textbook" for my online course
+    "Learn to Program in Java" dated on November 29, 2019.
+
+    I had completed, the Self Check, Problem 1:f “While Loops” based on the best possible textbook
+    instructions posted onto “GitHub” website, as the correct answer key dated on November 29, 2019.
+
+   Completed by Sarai Hannah Ajai
+ */
+public class BJP5SelfCheckChapter5Problem1f {
+    public static void main(String[] args) {
+        int x = 100;
+        while (x > 0) {
+
+            System.out.println(x / 10);
+            x = x / 2;
+        }
+    }
+}
